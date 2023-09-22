86d6f4b0fd8a823706effeea9ac4d874904ba39d
최윤석 <achoistic98@naver.com>
2023-09-23 01:50:20 +0900

    fix : remove typo in script

--- script.sh
+++ script.sh
@@ -8,6 +8,8 @@ kind create cluster --config ./manifests/kindconfig.yml
 kubectl create namespace istio-system
 kubectl create namespace devops-system
 
+
+
 kubectl apply -f ./manifests/istioconfig.yml
 
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.18/samples/addons/kiali.yaml 
@@ -17,8 +19,6 @@ kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.18/samp
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.18/samples/addons/prometheus.yaml
 
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.18/samples/addons/jaeger.yaml
-
-kubectl apply -f ./manifests/istiosystem.yml
 
 kubectl label namespace default istio-injection=enabled
 
