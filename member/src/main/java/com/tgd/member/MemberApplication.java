d5a1b788265962116aced76bb285e8581232a09d
scheduled-for-hacker <whtmdgus56@nate.com>
2023-09-23 22:38:51 +0900

    init: spring test

--- member/src/main/java/com/tgd/member/MemberApplication.java
+++ member/src/main/java/com/tgd/member/MemberApplication.java
@@ -4,17 +4,25 @@ import com.tgd.member.domain.member.entity.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MemberApplication {
 
     public static void main(String[] args) {
-        final String accountURL = " http://svc-golang/golang/hello";
+        final String accountURL = "http://svc-golang/golang/hello";
 
+        Test requestPayload = new Test("HELLO");
+        ResponseEntity<Test> response;
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<Test> response = restTemplate.postForEntity(accountURL, new Test("HELLO"), Test.class);
-        System.out.println(response.getBody());
+        try {
+            response = restTemplate.postForEntity(accountURL, requestPayload, Test.class);
+            System.out.println(response.getBody());
+        } catch (RestClientException e) {
+            System.out.println("[JAVA2] ERROR : error");
+            e.printStackTrace();
+        }
 
         SpringApplication.run(MemberApplication.class, args);
     }
