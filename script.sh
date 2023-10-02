8d317795e4a7276e71fc4008ca00ad0094884cbe
최윤석 <achoistic98@naver.com>
2023-10-03 04:04:08 +0900

    Init : external HA etcd cluster
    1. update script to create etcd cluster
    2. update docker-compose file to rename container
    3. create haproxy dir in /etcd
    4. update kind config file to use external etcd in KIND cluster

--- script.sh
+++ script.sh
@@ -3,21 +3,29 @@
 echo "script start time..."
 date
 
+# create external HA etcd cluster
+cd etcd
+docker-compose up --build -d
+cd ..
+
+# create KIND cluster
 kind create cluster --config ./manifests/kindconfig.yml
 
 sleep 20
 
+# deploy istio service mesh
 kubectl create namespace istio-system
-kubectl create namespace devops-system
-
 kubectl apply -f ./manifests/istioconfig.yml
+kubectl label namespace default istio-injection=enabled
 
+# deploy monitoring, tracing tools
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.18/samples/addons/kiali.yaml 
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.18/samples/addons/grafana.yaml
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.18/samples/addons/prometheus.yaml
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.18/samples/addons/jaeger.yaml
-kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.13.0/cert-manager.yaml
+# kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.13.0/cert-manager.yaml
 
+# build application container images
 cd frontend
 docker build -t frontend:tgd .
 cd ../golang
@@ -30,6 +38,7 @@ docker build -t mysql-golang:tgd -f Dockerfile.golang .
 cd ../member
 docker build -t member:tgd .
 
+# load container images to KIND cluster
 kind load docker-image mysql-member:tgd
 kind load docker-image mysql-golang:tgd
 kind load docker-image frontend:tgd
@@ -37,24 +46,23 @@ kind load docker-image golang:tgd
 kind load docker-image golang2:tgd
 kind load docker-image member:tgd
 
-kubectl label namespace default istio-injection=enabled
 
 cd ../manifests
 
+# deploy kubernetes resources
 kubectl apply -f pv.yml
 kubectl apply -f pvc.yml
 kubectl apply -f security/sa.yml
 kubectl apply -f security/ap.yml
 
+# deploy applications 
 kubectl apply -f https://github.com/rabbitmq/cluster-operator/releases/latest/download/cluster-operator.yml
 kubectl apply -f rabbitmq.yml
-
 kubectl apply -f mysql-member.yml
 kubectl apply -f mysql-golang.yml
-
+kubectl create namespace devops-system
 kubectl apply -n devops-system -f argocd.yml
 sleep 30
-
 kubectl apply -f security/vs-istiosystem.yml
 kubectl apply -f security/vs-default.yml
 kubectl apply -f frontend.yml
