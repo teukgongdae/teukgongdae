491df5b39f285ce1ca9266d437bff773da392782
최윤석 <achoistic98@naver.com>
2023-10-20 00:31:03 +0900

    Feat(frontend): Create FlippedClock component

---
+++ frontend/src/UI/FlippedClock.js
@@ -0,0 +1,40 @@
+import "./FlippedClock.css";
+
+const FlippedClock = (props) => {
+    return (
+        <div className="flippedClock">
+            <div className="flippedClock-time">
+                {props.start_time[0]}
+            </div>
+            <div className="flippedClock-time">
+                {props.start_time[1]}
+            </div>
+            :
+            <div className="flippedClock-time">
+                {props.start_time[2]}
+            </div>
+            <div className="flippedClock-time">
+                {props.start_time[3]}
+            </div>
+            -
+            <div className="flippedClock-time">
+                {props.end_time[0]}
+            </div>
+            <div className="flippedClock-time">
+                {props.end_time[1]}
+            </div>
+            :
+            <div className="flippedClock-time">
+                {props.end_time[2]}
+            </div>
+            <div className="flippedClock-time">
+                {props.end_time[3]}
+            </div>
+        </div>
+    );
+}
+
+export default FlippedClock;
+
+// props : start_time(string), end_time(string)
+// example : <FlippedClock start_time={spaceData.start_time} end_time={spaceData.end_time}/>
