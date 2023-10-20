698d762e1a3cb1868c0195c9dc856df545b13dd4
최윤석 <achoistic98@naver.com>
2023-10-21 04:13:48 +0900

    Feat(frontend): Create Footer component

---
+++ frontend/src/UI/Footer.js
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            © 2023. TEUKGONGDAE ALL RIGHTS RESERVED.
+        </div>
+    );
+}
+
+export default Footer;
